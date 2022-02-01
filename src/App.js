09d9d72efcd35edb03f38b20ae396ab55bb371ec
govind <gs5818475>
2022-02-01 15:28:57 +0530

    create-meme-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { MemeImage } from './MemeImage';
 
 function App() {
+  const [apiImages, setapiImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [meme, setMeme] = useState(null);
+
+  useEffect(()=>{
+      fetch('https://api.imgflip.com/get_memes')
+      .then(res =>  res.json())
+      .then(res => setapiImages(res.data.memes))
+      .catch(error => console.error(error));
+
+  },[]) // only on mount
+
+  const handleSelectedImageText = async (event) =>  {
+    event.preventDefault();
+    const params = {
+      template_id: selectedImage.id,
+      text0: topText,
+      text1: bottomText,
+      username: process.env.REACT_APP_USERNAME,
+      password: process.env.REACT_APP_PASSWORD,
+    }
+    const queryParams = new URLSearchParams(params).toString();
+    const response = await fetch(`https://api.imgflip.com/caption_image? ${queryParams}`)
+    const jsonResponse = await  response.json();
+    setMeme(jsonResponse.data);
+
+  }
+  if(meme) {
+    return (
+      <div>
+        <MemeImage image={meme}
+        />
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {selectedImage &&
+      <form onSubmit={(event)=> handleSelectedImageText(event)}>
+       <MemeImage 
+          image = {selectedImage}
+       />
+       <input
+       placeholder='top-text'
+       onChange={event => setTopText(event.target.value)}
+       value={topText}
+       />
+       <input
+       placeholder='bottom-text'
+       onChange={event => setBottomText(event.target.value)}
+       value={bottomText}
+       />
+       <button type='submit'>
+          Create Meme
+       </button>
+      </form>
+      }
+      {!selectedImage && (
+        <div>
+          <h1>Pick an image</h1>
+        {apiImages.map(apiImage=> {
+        return (
+          <MemeImage 
+              key={apiImage.id}
+              image = {apiImage}
+              onClick={()=>setSelectedImage(apiImage)}
+          />
+        )
+         })
+      }
+      </div>
+      )}
     </div>
   );
 }
